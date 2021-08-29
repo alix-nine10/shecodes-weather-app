8e057686d1bf344baf9ae10a2a70a5735360252d
alix-nine10 <52677025+alix-nine10@users.noreply.github.com>
2021-08-29 10:22:51 -0600

    Added JS file and then copied current project code into files

---
+++ script.js
@@ -0,0 +1,38 @@
+function showSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityHeading = document.querySelector("#todays-weather-city-name");
+
+  cityHeading.innerHTML = `${searchInput.value}`;
+  let apiKey = "4c4c2b7230f2b0c08d2e63af702d9fec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", showSearch);
+
+function showTemp(response) {
+  console.log(response.data.main.temp);
+  let tempRounded = Math.round(response.data.main.temp);
+  let tempElement = document.querySelector("#todays-temp");
+  tempElement.innerHTML = `${tempRounded}°C`;
+}
+
+function showDateTime() {
+  let h5 = document.querySelector("#date-heading");
+  let today = new Date();
+
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let day = days[today.getDay()];
+
+  h5.innerHTML = `${day} ${hour}:${minutes}`;
+}
+
+showDateTime();
